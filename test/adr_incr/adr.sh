b63261d0da704b380b20a7ff2259a402bb94afd2
barthess <barthess@yandex.ru>
2016-01-29 14:24:06 +0300

    Cosmetical improvements

--- test/adr_incr/adr.sh
+++ test/adr_incr/adr.sh
@@ -4,4 +4,4 @@ rm -f stim/*
 rmdir stim
 mkdir stim
 
-g++ -O0 --std=c++11 adr_test_gen.cpp -o adr_test_gen.elf && ./adr_test_gen.elf
+g++ -O0 --std=c++11 adr_test_gen.cpp && ./a.out
--- test/dadd/sum.sh
+++ test/dadd/sum.sh
@@ -4,4 +4,4 @@ rm -f stim/*
 rmdir stim
 mkdir stim
 
-g++ -O0 -Wall -Wextra --std=c++11 sum_test_gen.cpp -o sum_test_gen.elf && ./sum_test_gen.elf
+g++ -O0 -Wall -Wextra --std=c++11 sum_test_gen.cpp && ./a.out
