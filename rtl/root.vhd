3df0067c60929773f8f7a64182da5717ca639c47
barthess <barthess@yandex.ru>
2016-02-25 15:18:44 +0300

    Cosmetical improvements in root.vhd

--- rtl/root.vhd
+++ rtl/root.vhd
@@ -206,7 +206,8 @@ begin
   --
   -- clock sources
   --
-	clk_src : entity work.clk_src port map (
+	clk_src : entity work.clk_src 
+  port map (
 		CLK_IN1  => CLK_IN_27MHZ,
   	CLK_OUT1 => clk_200mhz,
 		CLK_OUT2 => clk_150mhz,
@@ -279,47 +280,47 @@ begin
   -- connect wishbone based LED strip
   --
   wb_led : entity work.wb_led
-    generic map (
-      AW => WB_AW,
-      DW => FSMC_DW
-    )
-    port map (
-      led   => LED_LINE,
+  generic map (
+    AW => WB_AW,
+    DW => FSMC_DW
+  )
+  port map (
+    led   => LED_LINE,
 
-      clk_i => clk_wb,
-      sel_i => wb_led_sel,
-      stb_i => wb_led_stb,
-      we_i  => wb_led_we,
-      err_o => wb_led_err,
-      ack_o => wb_led_ack,
-      adr_i => wb_led_adr,
-      dat_o => wb_led_dat_o,
-      dat_i => wb_led_dat_i
-    );
+    clk_i => clk_wb,
+    sel_i => wb_led_sel,
+    stb_i => wb_led_stb,
+    we_i  => wb_led_we,
+    err_o => wb_led_err,
+    ack_o => wb_led_ack,
+    adr_i => wb_led_adr,
+    dat_o => wb_led_dat_o,
+    dat_i => wb_led_dat_i
+  );
 
   --  
   -- FSMC to Wishbone adaptor
   --
   fsmc2wb : entity work.fsmc2wb 
-    generic map (
-      AW      => FSMC_AW,
-      DW      => FSMC_DW,
-      AWSEL   => 4,
-      AWSLAVE => WB_AW
-    )
-    port map (
-      clk_i => clk_wb,
-      external_err_o => STM_IO_WB_ERR_OUT,
-      external_mmu_err_o => STM_IO_MMU_ERR_OUT,
-      external_ack_o => STM_IO_WB_ACK_OUT,
-      
-      A   => FSMC_A,
-      D   => FSMC_D,
-      NCE => FSMC_NCE,
-      NOE => FSMC_NOE,
-      NWE => FSMC_NWE,
-      --NBL => FSMC_NBL,
-      
+  generic map (
+    AW      => FSMC_AW,
+    DW      => FSMC_DW,
+    AWSEL   => 4,
+    AWSLAVE => WB_AW
+  )
+  port map (
+    clk_i => clk_wb,
+    external_err_o => STM_IO_WB_ERR_OUT,
+    external_mmu_err_o => STM_IO_MMU_ERR_OUT,
+    external_ack_o => STM_IO_WB_ACK_OUT,
+    
+    A   => FSMC_A,
+    D   => FSMC_D,
+    NCE => FSMC_NCE,
+    NOE => FSMC_NOE,
+    NWE => FSMC_NWE,
+    --NBL => FSMC_NBL,
+    
 --      sel_o => wb_stub_sel,
 --      stb_o => wb_stub_stb,
 --      we_o  => wb_stub_we,
@@ -328,63 +329,63 @@ begin
 --      err_i => wb_stub_err,
 --      ack_i => wb_stub_ack,
 --      dat_i => wb_stub_dat_o
-      
-      sel_o(15 downto 16-WBSTUBS)         => wb_stub_sel,
-      sel_o(12)                           => wb_uart_sel,
-      sel_o(11)                           => wb_pwm_sel,
-      sel_o(10 downto 2)                  => wire_mul2wb_sel,
-      sel_o(1)                            => wb_led_sel,
-      sel_o(0)                            => wire_memtest_wb_sel,
-      
-      stb_o(15 downto 16-WBSTUBS)         => wb_stub_stb,
-      stb_o(12)                           => wb_uart_stb,
-      stb_o(11)                           => wb_pwm_stb,
-      stb_o(10 downto 2)                  => wire_mul2wb_stb,
-      stb_o(1)                            => wb_led_stb,
-      stb_o(0)                            => wire_memtest_wb_stb,
-      
-      we_o(15 downto 16-WBSTUBS)          => wb_stub_we,
-      we_o(12)                            => wb_uart_we,
-      we_o(11)                            => wb_pwm_we,
-      we_o(10 downto 2)                   => wire_mul2wb_we,
-      we_o(1)                             => wb_led_we,
-      we_o(0)                             => wire_memtest_wb_we,
-      
-      adr_o(WB_AW*16-1 downto WB_AW*13)     => wb_stub_adr,
-      adr_o(WB_AW*13-1 downto WB_AW*12)     => wb_uart_adr,
-      adr_o(WB_AW*12-1 downto WB_AW*11)     => wb_pwm_adr,
-      adr_o(WB_AW*11-1 downto WB_AW*2)      => wire_mul2wb_adr,
-      adr_o(WB_AW*2-1 downto WB_AW)         => wb_led_adr,
-      adr_o(WB_AW-1   downto 0)             => wire_memtest_wb_adr,
-      
-      dat_o(FSMC_DW*16-1 downto FSMC_DW*13) => wb_stub_dat_i,
-      dat_o(FSMC_DW*13-1 downto FSMC_DW*12) => wb_uart_dat_i,
-      dat_o(FSMC_DW*12-1 downto FSMC_DW*11) => wb_pwm_dat_i,
-      dat_o(FSMC_DW*11-1 downto FSMC_DW*2)  => wire_mul2wb_dat_i,
-      dat_o(FSMC_DW*2-1 downto FSMC_DW)     => wb_led_dat_i,
-      dat_o(FSMC_DW-1   downto 0)           => wire_memtest_wb_dat_i,
-      
-      err_i(15 downto 16-WBSTUBS)           => wb_stub_err,
-      err_i(12)                             => wb_uart_err,
-      err_i(11)                             => wb_pwm_err,
-      err_i(10 downto 2)                    => wire_mul2wb_err,
-      err_i(1)                              => wb_led_err,
-      err_i(0)                              => wire_memtest_wb_err,
-      
-      ack_i(15 downto 16-WBSTUBS)           => wb_stub_ack,
-      ack_i(12)                             => wb_uart_ack,
-      ack_i(11)                             => wb_pwm_ack,
-      ack_i(10 downto 2)                    => wire_mul2wb_ack,
-      ack_i(1)                              => wb_led_ack,
-      ack_i(0)                              => wire_memtest_wb_ack,
-      
-      dat_i(FSMC_DW*16-1 downto FSMC_DW*13) => wb_stub_dat_o,
-      dat_i(FSMC_DW*13-1 downto FSMC_DW*12) => wb_uart_dat_o,
-      dat_i(FSMC_DW*12-1 downto FSMC_DW*11) => wb_pwm_dat_o,
-      dat_i(FSMC_DW*11-1 downto FSMC_DW*2)  => wire_mul2wb_dat_o,
-      dat_i(FSMC_DW*2-1 downto FSMC_DW)     => wb_led_dat_o,
-      dat_i(FSMC_DW-1   downto 0)           => wire_memtest_wb_dat_o
-    );
+    
+    sel_o(15 downto 16-WBSTUBS)         => wb_stub_sel,
+    sel_o(12)                           => wb_uart_sel,
+    sel_o(11)                           => wb_pwm_sel,
+    sel_o(10 downto 2)                  => wire_mul2wb_sel,
+    sel_o(1)                            => wb_led_sel,
+    sel_o(0)                            => wire_memtest_wb_sel,
+    
+    stb_o(15 downto 16-WBSTUBS)         => wb_stub_stb,
+    stb_o(12)                           => wb_uart_stb,
+    stb_o(11)                           => wb_pwm_stb,
+    stb_o(10 downto 2)                  => wire_mul2wb_stb,
+    stb_o(1)                            => wb_led_stb,
+    stb_o(0)                            => wire_memtest_wb_stb,
+    
+    we_o(15 downto 16-WBSTUBS)          => wb_stub_we,
+    we_o(12)                            => wb_uart_we,
+    we_o(11)                            => wb_pwm_we,
+    we_o(10 downto 2)                   => wire_mul2wb_we,
+    we_o(1)                             => wb_led_we,
+    we_o(0)                             => wire_memtest_wb_we,
+    
+    adr_o(WB_AW*16-1 downto WB_AW*13)     => wb_stub_adr,
+    adr_o(WB_AW*13-1 downto WB_AW*12)     => wb_uart_adr,
+    adr_o(WB_AW*12-1 downto WB_AW*11)     => wb_pwm_adr,
+    adr_o(WB_AW*11-1 downto WB_AW*2)      => wire_mul2wb_adr,
+    adr_o(WB_AW*2-1 downto WB_AW)         => wb_led_adr,
+    adr_o(WB_AW-1   downto 0)             => wire_memtest_wb_adr,
+    
+    dat_o(FSMC_DW*16-1 downto FSMC_DW*13) => wb_stub_dat_i,
+    dat_o(FSMC_DW*13-1 downto FSMC_DW*12) => wb_uart_dat_i,
+    dat_o(FSMC_DW*12-1 downto FSMC_DW*11) => wb_pwm_dat_i,
+    dat_o(FSMC_DW*11-1 downto FSMC_DW*2)  => wire_mul2wb_dat_i,
+    dat_o(FSMC_DW*2-1 downto FSMC_DW)     => wb_led_dat_i,
+    dat_o(FSMC_DW-1   downto 0)           => wire_memtest_wb_dat_i,
+    
+    err_i(15 downto 16-WBSTUBS)           => wb_stub_err,
+    err_i(12)                             => wb_uart_err,
+    err_i(11)                             => wb_pwm_err,
+    err_i(10 downto 2)                    => wire_mul2wb_err,
+    err_i(1)                              => wb_led_err,
+    err_i(0)                              => wire_memtest_wb_err,
+    
+    ack_i(15 downto 16-WBSTUBS)           => wb_stub_ack,
+    ack_i(12)                             => wb_uart_ack,
+    ack_i(11)                             => wb_pwm_ack,
+    ack_i(10 downto 2)                    => wire_mul2wb_ack,
+    ack_i(1)                              => wb_led_ack,
+    ack_i(0)                              => wire_memtest_wb_ack,
+    
+    dat_i(FSMC_DW*16-1 downto FSMC_DW*13) => wb_stub_dat_o,
+    dat_i(FSMC_DW*13-1 downto FSMC_DW*12) => wb_uart_dat_o,
+    dat_i(FSMC_DW*12-1 downto FSMC_DW*11) => wb_pwm_dat_o,
+    dat_i(FSMC_DW*11-1 downto FSMC_DW*2)  => wire_mul2wb_dat_o,
+    dat_i(FSMC_DW*2-1 downto FSMC_DW)     => wb_led_dat_o,
+    dat_i(FSMC_DW-1   downto 0)           => wire_memtest_wb_dat_o
+  );
 
   --
   -- Memtest assistant
@@ -412,79 +413,78 @@ begin
   -- BRAM chunk for memtest purpose
   --
   bram_memtest : entity work.bram_memtest
-    PORT MAP (
-      -- port A connected to Wishbone wrapper
-      addra => wire_bram_a,
-      dina  => wire_bram_di,
-      douta => wire_bram_do,
-      wea   => wire_bram_we,
-      ena   => wire_bram_ce,
-      clka  => wire_bram_clk,
+  PORT MAP (
+    -- port A connected to Wishbone wrapper
+    addra => wire_bram_a,
+    dina  => wire_bram_di,
+    douta => wire_bram_do,
+    wea   => wire_bram_we,
+    ena   => wire_bram_ce,
+    clka  => wire_bram_clk,
 
-      -- port B connected to memtest assistant
-      addrb => wire_memtest_bram_a,
-      dinb  => wire_memtest_bram_do,
-      doutb => wire_memtest_bram_di,
-      enb   => wire_memtest_bram_ce,
-      web   => wire_memtest_bram_we,
-      clkb  => wire_memtest_bram_clk
-    );
+    -- port B connected to memtest assistant
+    addrb => wire_memtest_bram_a,
+    dinb  => wire_memtest_bram_do,
+    doutb => wire_memtest_bram_di,
+    enb   => wire_memtest_bram_ce,
+    web   => wire_memtest_bram_we,
+    clkb  => wire_memtest_bram_clk
+  );
   wire_bram_clk <= clk_wb;  
 
   --
   -- Connect memtest BRAM to wishbone adaptor
   --
   wb2bram : entity work.wb_bram
-    generic map (
-      WB_AW   => WB_AW,
-      DW      => FSMC_DW,
-      BRAM_AW => MEMTEST_BRAM_AW
-    )
-    port map (
-      -- BRAM
-      bram_clk_o => wire_bram_clk,
-      bram_adr_o => wire_bram_a,
-      bram_dat_i => wire_bram_do,
-      bram_dat_o => wire_bram_di,
-      bram_we_o  => wire_bram_we(0),
-      bram_en_o  => wire_bram_ce,
-      -- WB
-      clk_i => clk_wb,
-      sel_i => wire_memtest_wb_sel,
-      stb_i => wire_memtest_wb_stb,
-      we_i  => wire_memtest_wb_we,
-      err_o => wire_memtest_wb_err,
-      ack_o => wire_memtest_wb_ack,
-      adr_i => wire_memtest_wb_adr,
-      dat_o => wire_memtest_wb_dat_o,
-      dat_i => wire_memtest_wb_dat_i
-    );
+  generic map (
+    WB_AW   => WB_AW,
+    DW      => FSMC_DW,
+    BRAM_AW => MEMTEST_BRAM_AW
+  )
+  port map (
+    -- BRAM
+    bram_clk_o => wire_bram_clk,
+    bram_adr_o => wire_bram_a,
+    bram_dat_i => wire_bram_do,
+    bram_dat_o => wire_bram_di,
+    bram_we_o  => wire_bram_we(0),
+    bram_en_o  => wire_bram_ce,
+    -- WB
+    clk_i => clk_wb,
+    sel_i => wire_memtest_wb_sel,
+    stb_i => wire_memtest_wb_stb,
+    we_i  => wire_memtest_wb_we,
+    err_o => wire_memtest_wb_err,
+    ack_o => wire_memtest_wb_ack,
+    adr_i => wire_memtest_wb_adr,
+    dat_o => wire_memtest_wb_dat_o,
+    dat_i => wire_memtest_wb_dat_i
+  );
 
   --
   -- multiplicator with integrated BRAMs
   --
   wb_mtrx : entity work.wb_mtrx
-    generic map (
-      WB_AW => WB_AW,
-      WB_DW => FSMC_DW
-    )
-    port map (
-      rdy_o => STM_IO_MATH_RDY_OUT,
-      rst_i => STM_IO_MATH_RST_IN,
+  generic map (
+    WB_AW => WB_AW,
+    WB_DW => FSMC_DW
+  )
+  port map (
+    rdy_o => STM_IO_MATH_RDY_OUT,
+    rst_i => STM_IO_MATH_RST_IN,
 
-      clk_wb_i  => (others => clk_wb),
-      clk_mul_i => clk_mul,
-      
-      sel_i => wire_mul2wb_sel,
-      stb_i => wire_mul2wb_stb,
-      we_i  => wire_mul2wb_we,
-      err_o => wire_mul2wb_err,
-      ack_o => wire_mul2wb_ack,
-      adr_i => wire_mul2wb_adr,
-      dat_o => wire_mul2wb_dat_o,
-      dat_i => wire_mul2wb_dat_i
-    );
+    clk_wb_i  => (others => clk_wb),
+    clk_mul_i => clk_mul,
     
+    sel_i => wire_mul2wb_sel,
+    stb_i => wire_mul2wb_stb,
+    we_i  => wire_mul2wb_we,
+    err_o => wire_mul2wb_err,
+    ack_o => wire_mul2wb_ack,
+    adr_i => wire_mul2wb_adr,
+    dat_o => wire_mul2wb_dat_o,
+    dat_i => wire_mul2wb_dat_i
+  );
     
   -- connect debug modem
   modem_router : entity work.modem_router 
@@ -527,9 +527,9 @@ begin
     ubx_nrst  => UBLOX_NRST
   );
 
-        -- connect MOD telemetry
-        MODTELEM_RX_MNU <= MNU_TX_MSI_RX;
-        MNU_RX_MSI_TX <= MODTELEM_TX_MNU;
+  -- connect MOD telemetry
+  MODTELEM_RX_MNU <= MNU_TX_MSI_RX;
+  MNU_RX_MSI_TX <= MODTELEM_TX_MNU;
 
   --
 	-- raize ready flag for STM32
